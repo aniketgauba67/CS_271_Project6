66b000c68585ab78d49eb73002adb2fff1d51060
Andyreww <angulo_a1@denison.edu>
2024-04-17 22:12:14 -0400

    Cleaned .h file

--- graph.h
+++ graph.h
@@ -5,26 +5,27 @@
 #include <string>
 #include <unordered_map>
 
+using namespace std;
+
 template<typename D, typename K>
 class Graph {
 public:
-    Graph(const std::vector<K>& keys, const std::vector<D>& data, const std::vector<std::vector<K>>& edges);
-    
-    D* get(const K& key);
+    Graph(const vector<K>& keys, const vector<D>& data, const vector<vector<K>>& edges);
+    Vertex* get(const K& key);
     bool reachable(const K& u, const K& v);
     void bfs(const K& s);
     void print_path(const K& u, const K& v);
-    std::string edge_class(const K& u, const K& v);
+    string edge_class(const K& u, const K& v);
     void bfs_tree(const K& s);
 
 private:
     struct Vertex {
         D data;
         K key;
-        std::vector<Vertex*> adj;
+        vector<Vertex*> adj;
     };
     
-    std::unordered_map<K, Vertex> vertices;
+    unordered_map<K, Vertex> vertices;
 };
 
 #endif // GRAPH_H
