cd048b1c354a644288c1d112ef39c5bbb9d92c13
Andyreww <angulo_a1@denison.edu>
2024-04-17 22:11:52 -0400

    Finished Constructor

--- graph.cpp
+++ graph.cpp
@@ -1,1 +1,36 @@
+#include "graph.h"
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+// Constructor Class
+template<typename D, typename K>
+Graph<D, K>::Graph(const vector<K>& keys, const vector<D>& data, const vector<vector<K>>& edges) {
+    if (keys.size() != data.size() || keys.size() != edges.size()) {
+        return;
+    }
+
+    for (size_t i = 0; i < keys.size(); ++i) {
+        vertices[keys[i]].data = data[i];
+        vertices[keys[i]].key = keys[i];
+    }
+
+    for (size_t i = 0; i < edges.size(); ++i) {
+        for (const K& key : edges[i]) {
+            vertices[keys[i]].adj.push_back(&vertices[key]);
+        }
+    }
+}
+
+// Get Function
+template<typename D, typename K>
+Vertex* Graph<D, K>::get(const K& key) {
+
+}
+
+
+
+
 
