ff9f695ec940aeae731fa261c875c4e76e6e970a
Andyreww <angulo_a1@denison.edu>
2024-04-18 23:18:52 -0400

    Finished get functions and reachable function

--- graph.cpp
+++ graph.cpp
@@ -1,6 +1,7 @@
 #include "graph.h"
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <map>
 
 
@@ -27,16 +28,16 @@ Graph<D, K>::Graph(const vector<K>& keys, const vector<D>& data, const vector<ve
 
 // Get Function
 template<typename D, typename K>
-Vertex<D, K>* Graph<D, K>::get(K key) const{
-    int length = vetices.size();
-
-    for(int i = 0; i<len; i++){
-        if(vertices[i]->key == key){
-            return vertices[i];
+const typename Graph<D, K>::Vertex* Graph<D, K>::get(K key) const{
+    for(const auto& pair : vertices){
+        if(pair.second.key == key){
+            return &pair.second;
         }
     }
     return nullptr;
 }
+
+
 
 
 // edge exists functions
@@ -51,6 +52,41 @@ bool Graph<D, K>::edge_exists(const K& u, const K& v) {
             return true;
         }
     }
+    return false;
+}
+
+template<typename D, typename K>
+bool Graph<D, K>::reachable(const K& u, const K& v) {
+    // Create a map to store visited vertices
+    unordered_map<K, bool> visited;
+
+    // Create a queue for BFS
+    queue<Vertex*> queue;
+
+    // Mark the current node as visited and enqueue it
+    visited[u] = true;
+    queue.push(&vertices[u]);
+
+    while(!queue.empty()) {
+        // Dequeue a vertex from queue
+        Vertex* vertex = queue.front();
+        queue.pop();
+
+        // Get all adjacent vertices of the dequeued vertex
+        // If an adjacent vertex has not been visited, then mark it visited and enqueue it
+        for(auto i = vertex->adj.begin(); i != vertex->adj.end(); ++i) {
+            if (!visited[(*i)->key]) {
+                // If this adjacent node is the destination node, return true
+                if ((*i)->key == v)
+                    return true;
+
+                visited[(*i)->key] = true;
+                queue.push(*i);
+            }
+        }
+    }
+
+    // If BFS is complete without visiting v
     return false;
 }
 
