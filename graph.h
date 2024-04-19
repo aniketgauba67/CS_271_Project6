143ea92f15581a240139dde928ce82523105eedd
Andyreww <angulo_a1@denison.edu>
2024-04-18 23:18:04 -0400

    Fixed Syntax that caused error in testfile

--- graph.h
+++ graph.h
@@ -11,8 +11,14 @@ using namespace std;
 template<typename D, typename K>
 class Graph {
 public:
+    struct Vertex {
+        D data;
+        K key;
+        vector<Vertex*> adj;
+    };
+
     Graph(const vector<K>& keys, const vector<D>& data, const vector<vector<K>>& edges);
-    Vertex* get(K key) const;
+    const Vertex* get(K key) const;
     bool reachable(const K& u, const K& v);
     void bfs(const K& s);
     void print_path(const K& u, const K& v);
@@ -20,13 +26,7 @@ public:
     void bfs_tree(const K& s);
     bool edge_exists(const K& u, const K& v);
 
-private:
-    struct Vertex {
-        D data;
-        K key;
-        vector<Vertex*> adj;
-    };
-    
+private:    
     unordered_map<K, Vertex> vertices;
 };
 
