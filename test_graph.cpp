fc3c9335b039a5c5b99df910eac9d27a83333cc1
Andrew Angulo <98863478+Andyreww@users.noreply.github.com>
2024-04-22 16:59:10 -0400

    Add files via upload

--- test_graph.cpp
+++ test_graph.cpp
@@ -1,10 +1,3 @@
-//
-//  test_graph_example.cpp
-//  CS 271 Graph Project: Example Test File
-//
-//  Created by Dr. Stacey Truex
-//
-
 #include <fstream>
 #include <sstream>
 #include "graph.cpp"
@@ -43,22 +36,68 @@ Graph<string, string> *generate_graph(string fname)
     return G;
 }
 
+void test_is_descendant(Graph<string, string> *G)
+{
+    try
+    {
+        if (!G->is_descendant("T", "V"))
+        {
+            cout << "Incorrectly identified \"V\" as not a descendant of \"T\"" << endl;
+        }
+        if (G->is_descendant("V", "T"))
+        {
+            cout << "Incorrectly identified \"T\" as a descendant of \"V\"" << endl;
+        }
+        if (G->is_descendant("S", "A"))
+        {
+            cout << "Incorrectly identified non-existant vertex \"A\" as a descendant of \"S\"" << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error testing is_descendant : " << e.what() << endl;
+    }
+}
+
+void test_dfs(Graph<string, string> *G)
+{
+    try
+    {
+        G->dfs("T");
+        string vertices[8] = {"V", "R", "S", "W", "T", "X", "U", "Y"};
+        int discovery_times[8] = {1, 2, 3, 4, 5, 6, 7, 8}; // replace with actual discovery times
+        int finish_times[8] = {16, 15, 14, 13, 12, 11, 10, 9}; // replace with actual finish times
+        for (int i = 0; i < 8; i++)
+        {
+            if (G->get(vertices[i]) == nullptr || G->get(vertices[i])->d != discovery_times[i] || G->get(vertices[i])->f != finish_times[i])
+            {
+                cout << "Incorrect dfs result. Vertex " << vertices[i] << " should have discovery time " << discovery_times[i] << " and finish time " << finish_times[i] << endl;
+            }
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error testing is_descendant : " << e.what() << endl;
+    }
+
+}
+
 void test_get(Graph<string, string> *G)
 {
     try
     {
         if (G->get("S") == nullptr || G->get("S")->data != "S data")
         {
-            cout << "Incorrect result getting vertex \"s\"" << endl;
+            cout << "Incorrect result getting vertex \"S\"" << endl;
         }
-        if (G->get("a") != nullptr)
+        if (G->get("A") != nullptr)
         {
-            cout << "Incorrect result getting non-existant vertex \"a\"" << endl;
+            cout << "Incorrect result getting non-existent vertex \"A\"" << endl;
         }
     }
-    
-    catch (exception& e) {
-        cerr << "Error testing empty graph: " << e.what() << endl;
+    catch (exception &e)
+    {
+        cerr << "Error getting vertex from graph : " << e.what() << endl;
     }
 }
 
@@ -70,13 +109,13 @@ void test_reachable(Graph<string, string> *G)
         {
             cout << "Incorrectly identified adjacent vertex \"V\" as unreachable from \"R\"" << endl;
         }
-        if (!G->reachable("X", "W"))
+        if (!G->reachable("T", "W"))
         {
-            cout << "Incorrectly identified \"W\" as unreachable from \"X\"" << endl;
+            cout << "Incorrectly identified \"W\" as unreachable from \"T\"" << endl;
         }
         if (G->reachable("S", "A"))
         {
-            cout << "Incorrectly identified non-existant vetex \"A\" as reachable from \"S\"" << endl;
+            cout << "Incorrectly identified non-existent vertex \"A\" as reachable from \"S\"" << endl;
         }
     }
     catch (exception &e)
@@ -91,12 +130,12 @@ void test_bfs(Graph<string, string> *G)
     {
         G->bfs("T");
         string vertices[8] = {"V", "R", "S", "W", "T", "X", "U", "Y"};
-        int distances[8] = {3, 2, 1, 1, 0, 2, 1, 2};
+        int distances[8] = {3, 2, 1, 1, 0, 2, 1, 2}; // replace with actual distances
         for (int i = 0; i < 8; i++)
         {
             if (G->get(vertices[i]) == nullptr || G->get(vertices[i])->distance != distances[i])
             {
-                cout << "Incorrect bfs result. Vertex " << vertices[i] << " should have distance " << distances[i] << " from source vertex \"t\"" << endl;
+                cout << "Incorrect bfs result. Vertex " << vertices[i] << " should have distance " << distances[i] << " from source vertex \"T\"" << endl;
             }
         }
     }
@@ -142,7 +181,7 @@ void test_edge_class(Graph<string, string> *G)
         e_class = G->edge_class("R", "U"); // no edge
         if (e_class != "no edge")
         {
-            cout << "Misidentified non-existant edge (\"R\", \"U\") as : " << e_class << endl;
+            cout << "Misidentified non-existent edge (\"R\", \"U\") as : " << e_class << endl;
         }
         e_class = G->edge_class("T", "W"); // forward edge
         if (e_class != "forward edge")
@@ -152,7 +191,7 @@ void test_edge_class(Graph<string, string> *G)
         e_class = G->edge_class("T", "S"); // cross edge
         if (e_class != "cross edge")
         {
-            cout << "Misidentified forward edge (\"T\", \"S\") as : " << e_class << endl;
+            cout << "Misidentified cross edge (\"T\", \"S\") as : " << e_class << endl;
         }
     }
     catch (exception &e)
@@ -177,14 +216,35 @@ void test_bfs_tree(Graph<string, string> *G)
     }
     catch (exception &e)
     {
-        cerr << "Error testing bfs tree : " << e.what() << endl;
+        cerr << "Error bfstree : " << e.what() << endl;
     }
 }
 
+void test_edge_exists(Graph<string, string> *G)
+{
+    try
+    {
+        if (!G->edge_exists("R", "V"))
+        {
+            cout << "Incorrectly identified edge (\"R\", \"V\") as non-existent" << endl;
+        }
+        if (G->edge_exists("V", "R"))
+        {
+            cout << "Incorrectly identified non-existent edge (\"V\", \"R\") as existent" << endl;
+        }
+        if (G->edge_exists("S", "A"))
+        {
+            cout << "Incorrectly identified edge to non-existent vertex (\"S\", \"A\") as existent" << endl;
+        }
+    }
+    catch (exception &e)
+    {
+        cerr << "Error testing edge_exists : " << e.what() << endl;
+    }
+}
 
 int main()
 {
-
     Graph<string, string> *G = generate_graph("graph_description.txt");
     test_get(G);
     test_reachable(G);
@@ -192,6 +252,9 @@ int main()
     test_print_path(G);
     test_edge_class(G);
     test_bfs_tree(G);
+    test_is_descendant(G);
+    
+    test_edge_exists(G);
 
     cout << "Testing completed" << endl;
 
